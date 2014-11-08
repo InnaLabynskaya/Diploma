46636c0ca38be6c4ebdbee39f4797915d2894bfd
InnaLabynskaya <innalabynskaya@gmail.com>
2014-11-08 17:06:03 +0200

    Added Shop class

--- Diploma/Diploma/Shop.cpp
+++ Diploma/Diploma/Shop.cpp
@@ -1,1 +1,62 @@
 #include "Shop.h"
+#include <iostream>
+using namespace std;
+
+Shop::Shop(Category* category):m_category(category)
+{
+	load();
+}
+
+void Shop::showProducts()
+{
+	m_category->output()->show(m_products);
+}
+
+void Shop::showProduct(unsigned int index)
+{
+	if(index < m_products.size())
+		m_category->output()->show(m_products[index]);
+	else
+		cout<<"There is no product with this index\n";
+}
+
+void Shop::removeProducts()
+{
+	vector<Product*>::iterator it;
+	for(it = m_products.begin(); it != m_products.end(); it++)
+	{
+		delete *it;
+	}
+	m_products.clear();
+}
+
+void Shop::removeProduct(unsigned int index)
+{
+	if(index < m_products.size())
+	{
+		delete m_products[index];
+		m_products.erase(m_products.begin() + index);
+	}
+	else
+		cout<<"There is no product with this index\n";
+}
+
+void Shop::addProduct()
+{
+	m_products.push_back(m_category->input()->createProduct());
+}
+
+void Shop::load()
+{
+	m_category->load()->load(m_products);
+}
+
+void Shop::save()
+{
+	m_category->save()->save(m_products);
+}
+
+Shop::~Shop()
+{
+	removeProducts();
+}
--- Diploma/Diploma/Shop.h
+++ Diploma/Diploma/Shop.h
@@ -1,9 +1,24 @@
 #ifndef __SHOP__
 #define __SHOP__
+#include "Category.h"
+#include "Product.h"
+#include <vector>
 
 class Shop
 {
-
+public:
+	Shop(Category* category);
+	void showProducts();
+	void showProduct(unsigned int index);
+	void removeProducts();
+	void removeProduct(unsigned int index);
+	void addProduct();
+	void load();
+	void save();
+	~Shop();
+private:
+	std::vector<Product*> m_products;
+	Category* m_category;
 };
 
 #endif
